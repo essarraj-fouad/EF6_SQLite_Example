a32c675240fc9a5b88582e631f52581012d3e04b
ESSARRAJ <essarraj.fouad@gmail.com>
2017-12-29 16:16:48 +0100

    first commit

---
+++ ConsoleApp2/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using App.Entities;
+using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() :
+            base(new SQLiteConnection()
+            {
+                ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = "test.db", ForeignKeys = true }.ConnectionString
+            },true)
+        {
+        }
+        
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<DatabaseContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+
+        }
+
+        public DbSet<Trainee> Trainees { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseContext db = new DatabaseContext();
+
+            db.Trainees.Add(new App.Entities.Trainee());
+                db.SaveChanges();
+               
+
+        }
+    }
+}
